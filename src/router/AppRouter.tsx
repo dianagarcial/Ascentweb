e394e927935abc24135833ed42a9a05cd107107e
Juan-Camilo-Tabarquino <juancatabarquino@gmail.com>
2022-12-07 15:06:55 -0500

    Inicios

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,12 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import { HomePage } from "../components";
 
 export const AppRouter = () => {
 
     return (
         <Routes>
-
-
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/*" element={ <Navigate to ="/"/> }/>
         </Routes>
     )
