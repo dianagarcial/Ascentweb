69fabca67e50db2ec5509b1fa877d4ae3d7f4acb
dianagarcial <diamagalo@gmail.com>
2022-12-08 13:27:39 -0500

    overflow

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,6 +1,7 @@
 import { NavBar } from "../Navbar"
 import { ConteServicios } from "./conteServicios"
 import { ConteTecnogias } from "./conteTecnologias"
+import '../../styles/index.css'
 
 export const HomePage = () => {
   return (
